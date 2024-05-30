22359e20693e5b7c4a9df5c033504dff2dd7df5f
minhajul <dev.minhajul@gmail.com>
2024-05-30 21:57:22 +0600

    added readme file and images folder test screenshot

---
+++ README.md
@@ -0,0 +1,3 @@
+<div align="center">
+    <h3>this is Puppeteer Practice Repository!</h3>
+ </div>
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const puppeteer = require("puppeteer");
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto("https://google.com/");
+    await page.goto("https://www.minhajul.com/");
 
     await page.setViewport({ width: 1080, height: 1024 });
 
-    await page.screenshot({ path: "google.png" });
+    await page.screenshot({ path: "./images/minhaj.png" });
 
     await browser.close();
   } catch (error) {
