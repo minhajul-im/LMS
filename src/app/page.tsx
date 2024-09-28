3691a827cfe4db4b72a62f6a2c260e2c8b9df729
minhajul <dev.minhajul@gmail.com>
2024-09-28 20:36:16 +0600

    started new project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <main className="container mx-auto">
+      <h1 className="pt-10">hello</h1>
+      <Button>hello bangladesh</Button>
+    </main>
+  );
+};
+
+export default HomePage;
